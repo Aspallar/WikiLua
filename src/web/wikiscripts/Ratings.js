ebec4802dc7244961b7f889d804d53c6e0b9bc1d
Aspallar <shadowthrone99@outlook.com>
2018-03-16 03:38:27 +0000

    Updating decklists page added

--- src/web/wikiscripts/Ratings.js
+++ src/web/wikiscripts/Ratings.js
@@ -1,332 +1,249 @@
-/* jshint -W004 */ // allow redifinition of vars
+// ==========================================================================
+// Start: Deck Ratings
+//    1. Supports the rating 'stars' on deck pages
+//    2. Updates the rating column on the decklists page
+// Version 1.0.0
+// Author: Aspallar
+//
+// ** Please dont edit this code directly in the wikia.
+// ** Instead use the git repository https://github.com/Aspallar/WikiLua
+//
+// This code was inspired by the rating system used on http://de.sonic.wikia.com
+//
+(function ($) {
+    /* global mw */
+    'use strict';
 
-console.log("Ratings Loading");
-addOnloadHook(function () {
-
-    if ($('.rating').length == 0)
+    if (document.getElementById('mdw-rating') === null && $('.mdw-decklist').length === null)
         return;
 
-    console.log("Ratings initialising");
-    //if ($.inArray("Spiele", wgCategories) > -1) {
-    if (true) {
-        var pageName = 'Ratings:' + wgPageName;
-        disableEdit = false;
-        var symbolInactive = new Image();
-        symbolInactive.src = 'http://static1.wikia.nocookie.net/sonicwiki/de/images/9/91/SonicGamesRatingInactive.png';
-        var symbolActive = new Image();
-        symbolActive.src = 'http://static4.wikia.nocookie.net/sonicwiki/de/images/9/95/SonicGamesRatingActive.png';
- 
-        if (!wgUserName) {
-            $.getJSON("http://smart-ip.net/geoip-json?callback=?", function (data) {}).done(function (data) {
-                var userIP = data.host;
-                getReview(pageName, userIP);
-            });
-        } else {
-            var userIP = wgUserName;
-            console.log("user=" + userIP);
-            getReview(pageName, userIP);
-        }
- 
-    }
- 
-    saveRating = false;
-    $('.rating').mouseenter(function () {
-        var currentRating = parseInt($(this).attr('id').split("-")[1]);
- 
-        $('.rating').each(function () {
-            var currentRatingEach = parseInt($(this).attr('id').split("-")[1]);
-            if (currentRatingEach <= currentRating && saveRating == false) {
-                $(this).addClass('rating-active');
-            }
-        });
-    });
- 
-    $('.rating').mouseleave(function () {
-        if (saveRating == false) {
-            $('.rating').attr('class', 'rating');
-        }
-    });
- 
-    $(".rating").on("click", function () {
-        saveRating = true;
-        actualRating = parseInt($(this).attr('id').split("-")[1]);
- 
-        if (!wgUserName) {
-            $.getJSON("http://smart-ip.net/geoip-json?callback=?", function (data) {}).done(function (data) {
-                var userIP = data.host;
-                var pageName = 'Ratings:' + wgPageName;
- 
-                var contentText = '<noinclude>' + userIP + ':</noinclude>' + actualRating;
- 
-                if (disableEdit == false) {
-                    submitReview(pageName, contentText, userIP, actualRating);
-                }
-            });
-        } else {
-            var userIP = wgUserName;
-            var pageName = 'Ratings:' + wgPageName;
- 
-            var contentText = '<noinclude>' + userIP + ':</noinclude>' + actualRating;
- 
-            if (disableEdit == false) {
-                submitReview(pageName, contentText, userIP, actualRating);
-            }
-        }
- 
-        if (saveRating == true) {
-            $('.rating').attr('class', 'rating');
-            $('.rating').each(function () {
-                var currentRatingEach = parseInt($(this).attr('id').split("-")[1]);
-                if (currentRatingEach <= actualRating) {
-                    $(this).addClass('rating-active');
-                }
-            });
-        }
-    });
-});
- 
-function showError(msg) {
-    console.log(msg);
-}
- 
-function callAPI(data, method, callback) {
-    data.format = 'json';
-    $.ajax({
-        data: data,
-        dataType: 'json',
-        url: wgScriptPath + '/api.php',
-        type: method,
-        success: function (response) {
-            if (response.error) {
-                showError('API error: ' + response.error.info);
-            } else {
-                callback(response);
-            }
-        },
-        error: function (xhr, error) {
-            showError('AJAX error: ' + error);
-        },
-        timeout: 10000 // msec
-    });
-}
- 
-function submitReview(reviewPageName, reviewRating, userIP, rating) {
-    console.log("submitReview");
-    console.log(reviewPageName);
-    //console.log("submitReview " + reviewPageName + " " + reviewRating + " " + userIP + " " + rating);
-    disableEdit = true;
-    setTimeout(function () {
-        disableEdit = false;
-    }, 500);
-    callAPI({
-        'action': 'query',
-        'prop': 'info|revisions',
-        'intoken': 'edit',
-        'titles': reviewPageName,
-        'rvprop': 'content',
-        'rvlimit': '1'
-    }, 'GET', function (response) {
-        var pages = response.query.pages;
-        var page = null;
- 
-        for (var i in pages) {
-            page = pages[i];
-        }
- 
-        if (page.missing == "") {
-            console.log("page.missing is empty");
-            var content = reviewRating;
- 
-            callAPI({
-                'minor': 'yes',
-                'summary': 'rating test',
-                'action': 'edit',
-                'title': reviewPageName,
-                'startimestamp': page.starttimestamp,
-                'token': page.edittoken,
-                'watchlist': 'unwatch',
-                'text': content
-            }, 'POST', function (response) {
-                if (response.edit.result == 'Success') {
- 
-                    averageArray = [];
-                    var averageRatingSymbols = Math.round(rating);
-                    $('.rating-average').each(function () {
-                        var currentAverageRatingEach = parseInt($(this).attr('id').split("-")[1]);
-                        if (currentAverageRatingEach <= averageRatingSymbols) {
-                            $(this).addClass('rating-average-active');
-                        }
-                    });
-                    $('.rating-people').text(1);
- 
-                    $('.rating-total').attr('data-rating', rating);
- 
-                    averageArray.push(parseInt(rating));
-                } else {
- 
-                }
-            });
-        } else {
-            console.log("page.missing is not empty");
+    var ratingsDataPageName = 'Ratings:DeckRatings';
 
-            var content = page.revisions[0]['*'];
- 
-            if ((page.length) == 0) {
-                var content = reviewRating;
-            } else {
-                if (content.match(userIP)) {
-                    var numbers = new RegExp(userIP + '\:\<\/noinclude>[0-9]([0-9])?', 'm');
-                    var content = content.replace(numbers, userIP + '\:\<\/noinclude>' + rating);
-                } else {
-                    var content = content + ' + ' + reviewRating;
-                }
+    function stripDeckPrefix(deckName) {
+        if (deckName.substring(0, 6) === 'Decks/')
+            return deckName.substring(6);
+        return deckName;
+    }
+
+    function getDeckName() {
+        var name = mw.config.get('wgPageName');
+        name = stripDeckPrefix(name);
+        return name;
+    }
+
+    function calcScore(rating) {
+        return Math.round(rating.total / rating.votes);
+    }
+
+    function showError(error) {
+        console.log(error);
+    }
+
+    function wikiApiCall(data, method, cbSuccess) {
+        data.format = 'json';
+        $.ajax({
+            data: data,
+            dataType: 'json',
+            url: mw.config.get('wgScriptPath') + '/api.php',
+            type: method,
+            success: function (response) {
+                if (response.error === undefined)
+                    cbSuccess(response);
+                else
+                    showError('API error in wikiApiCall: ' + response.error.info);
+            },
+            error: function (xhr, error) {
+                showError('AJAX error in wikiApiCall: ' + error);
+            },
+            timeout: 10000 
+        });
+    }
+
+    function findRating(name, ratingData) {
+        for (var k = 0, l = ratingData.length; k < l; k++) {
+            if (ratingData[k].name === name) {
+                return ratingData[k];
             }
- 
-            callAPI({
+        }
+        return null;
+    }
+
+    function getRating(name, ratingData) {
+        var rating = findRating(name, ratingData);
+        if (rating === null) {
+            rating = {name:name, total:0, votes:0};
+            ratingData.push(rating);
+        }
+        return rating;
+    }
+
+    function updateRatingUiValue(rating) {
+        $('.mdw-rating-score').each(function () {
+            var $this = $(this);
+            if (rating >= parseInt($this.attr('data-rating'), 10))
+                $this.addClass('mdw-rating-active');
+            else
+                $this.removeClass('mdw-rating-active');
+        });
+    }
+
+    function getDataFromPage(page) {
+        var content = page.revisions[0]['*'];
+        return JSON.parse(content);
+    }
+
+    function getPageFromResponse(response) {
+        /* jshint -W089 */ // no need for hasOwnProperty guard here
+        var pages = response.query.pages;
+        var page;
+        for (var i in pages)
+            page = pages[i];
+        return page;
+    }
+
+    function fetchRatingPage(cbDone) {
+        wikiApiCall({
+            'action': 'query',
+            'prop': 'info|revisions',
+            'intoken': 'edit',
+            'titles': ratingsDataPageName,
+            'rvprop': 'content',
+            'rvlimit': '1'
+        },
+        'GET',
+        function (response) {
+            var page = getPageFromResponse(response);
+            cbDone(page);
+        });
+    }
+
+    function fetchRatingData(cbDone) {
+        fetchRatingPage(function (page) {
+            var data = getDataFromPage(page);
+            cbDone(data);
+        });
+    }
+
+    function addScore(rating, score) {
+        rating.total += score;
+        ++rating.votes;
+    }
+
+    function updateRating(deckName, score) {
+        fetchRatingPage(function (page) {
+            var data = getDataFromPage(page);
+            var rating = getRating(deckName, data);
+            addScore(rating, score);
+            var newContent = JSON.stringify(data);
+            updateRatingUiValue(calcScore(rating));
+            wikiApiCall({
                 'minor': 'yes',
-                'summary': 'Rating test 2',
+                'summary': 'Rating update (automatic)',
                 'action': 'edit',
-                'title': reviewPageName,
+                'title': ratingsDataPageName,
                 'basetimestamp': page.revisions[0].timestamp,
                 'startimestamp': page.starttimestamp,
                 'token': page.edittoken,
                 'watchlist': 'unwatch',
-                'text': content
-            }, 'POST', function (response) {
-                if (response.edit.result == 'Success') {
- 
- 
-                    var newAverageArray = [];
-                    var newAverageArray = averageArray;
-                    var stopSplice = false;
-                    var oldRating = $('.rating-total').attr('data-rating');
- 
-                    for (var i = newAverageArray.length - 1; i >= 0; i--) {
-                        if (newAverageArray[i] == oldRating && stopSplice == false) {
-                            newAverageArray.splice(i, 1);
-                            var stopSplice = true;
-                        }
-                    }
- 
-                    newAverageArray.push(parseInt(rating));
- 
-                    sumRatingNew = 0;
-                    for (var x = 0; x < newAverageArray.length; x++) {
-                        sumRatingNew = sumRatingNew + newAverageArray[x];
-                    }
- 
-                    averageRatingNew = sumRatingNew / newAverageArray.length;
- 
-                    var averageRatingSymbols = Math.round(averageRatingNew);
-                    $('.rating-average').attr('class', 'rating-average');
-                    $('.rating-average').each(function () {
-                        var currentAverageRatingEach = parseInt($(this).attr('id').split("-")[1]);
-                        if (currentAverageRatingEach <= averageRatingSymbols) {
-                            $(this).addClass('rating-average-active');
-                        }
-                    });
-                    $('.rating-people').text(newAverageArray.length);
- 
- 
-                    $('.rating-total').attr('data-rating', rating);
- 
-                } else {
- 
-                }
+                'text': newContent
+            },
+            'POST',
+            function (response) {
+                if (response.edit.result !== 'Success')
+                    showError('Update fail in updateRating: ' + response.edit.result);
             });
-        }
-    });
-}
-
-function onReviewReceived(rawData, isNew){
-    
-}
-
-function fetchRatingData(cb) {
-    callAPI({
-        'action': 'query',
-        'prop': 'info|revisions',
-        'intoken': 'edit',
-        'titles': reviewPageName,
-        'rvprop': 'content',
-        'rvlimit': '1'
-    }, 'GET', function (response) {
-        
+        });
     }
-}
 
- 
-function getReview(reviewPageName, userIP) {
-    console.log("getReview");
-    console.log("getReview" + reviewPageName + " " + userIP);
-    callAPI({
-        'action': 'query',
-        'prop': 'info|revisions',
-        'intoken': 'edit',
-        'titles': reviewPageName,
-        'rvprop': 'content',
-        'rvlimit': '1'
-    }, 'GET', function (response) {
-        console.log("Review query recieved");
-        var pages = response.query.pages;
-        var page = null;
- 
-        for (var i in pages) {
-            page = pages[i];
-        }
- 
-        if (page.missing == "") {
-            console.log("Page missing");
-            $('.rating-people').text(0);
-        } else {
-            var content = page.revisions[0]['*'];
- 
-            var contentArray = content.split("+");
- 
-            averageArray = [];
-            $(contentArray).each(function (index, value) {
-                var averageRatingItem = contentArray[index];
- 
-                var averageRatingEachExp = new RegExp('\:\<\/noinclude\>[0-9]+', 'm');
-                var averageRatingEach = averageRatingItem.match(averageRatingEachExp)[0].replace(':</noinclude>', '');
- 
-                averageArray.push(parseInt(averageRatingEach));
- 
-                if (contentArray[index].indexOf(userIP) > -1) {
-                    var ratingArrayItem = contentArray[index];
- 
-                    var ratingRegExp = new RegExp('\:\<\/noinclude\>[0-9]+', 'm');
-                    ratingFinal = ratingArrayItem.match(ratingRegExp)[0].replace(':</noinclude>', '');
- 
-                    $('.rating-total').attr('data-rating', ratingFinal);
- 
-                    $('.rating').each(function () {
-                        var currentRatingEach = parseInt($(this).attr('id').split("-")[1]);
-                        if (currentRatingEach <= ratingFinal) {
-                            $(this).addClass('rating-active');
-                        }
-                        saveRating = true;
-                    });
-                }
-            });
-            sumRating = 0;
-            for (var x = 0; x < averageArray.length; x++) {
-                sumRating = sumRating + averageArray[x];
+    function onRatingMouseEnter() {
+        /* jshint -W040 */ // allow old school jquery use of this
+        var rating = parseInt($(this).attr('data-rating'), 10);
+        $('.mdw-rating').each(function () {
+            var $this = $(this);
+            var thisRating = parseInt($this.attr('data-rating'), 10);
+            if (thisRating <= rating) {
+                $this.addClass('mdw-rating-active');
             }
- 
-            averageRating = sumRating / averageArray.length;
- 
-            var averageRatingSymbols = Math.round(averageRating);
-            $('.rating-average').each(function () {
-                var currentAverageRatingEach = parseInt($(this).attr('id').split("-")[1]);
-                if (currentAverageRatingEach <= averageRatingSymbols) {
-                    $(this).addClass('rating-average-active');
+        });
+    }
+
+    function onRatingMouseLeave() {
+        $('.mdw-rating').removeClass('mdw-rating-active');
+    }
+
+    function onRatingClick() {
+        /* jshint -W040 */ // allow old school jquery use of this
+        var rating = parseInt($(this).attr('data-rating'), 10);
+        $('#mdw-rating').css('display', 'none');
+        updateRating(getDeckName(), rating);
+    }
+
+    function initializeRating() {
+        fetchRatingData(function (data) {
+            var rating = findRating(getDeckName(), data);
+            if (rating !== null)
+                updateRatingUiValue(calcScore(rating));
+        });
+    }
+
+    function wireUpEvents() {
+        $('.mdw-rating')
+            .mouseenter(onRatingMouseEnter)
+            .mouseleave(onRatingMouseLeave)
+            .click(onRatingClick);
+    }
+
+    function addDataRatingAttributes(className) {
+        $(className).each(function (index, element) {
+            $(element).attr('data-rating', index + 1);
+        });
+    }
+
+    function addAllDataRatingAttributes() {
+        addDataRatingAttributes('.mdw-rating-score');
+        addDataRatingAttributes('.mdw-rating');
+    }
+
+    function getDeckNames(decklists) {
+        var names = [];
+        decklists.find('td:nth-child(1) a').each(function () {
+            names.push(stripDeckPrefix($(this).attr('title')));
+        });
+        return names;
+    }
+
+    function updateRatingColumn() {
+        fetchRatingData(function (data) {
+            var deckRow = 0;
+            var decklists = $('.mdw-decklist');
+            var names = getDeckNames(decklists);
+            decklists.find('td:nth-child(6)').each(function () {
+                var rating = findRating(names[deckRow], data);
+                if (rating !== null) {
+                    $(this).html(calcScore(rating));
                 }
-                $('.rating-people').text(averageArray.length);
+                ++deckRow;
             });
- 
-            $('.rating-average').attr('data-amount', averageArray.length);
+        });
+    }
+
+    function initializeStars() {
+        addAllDataRatingAttributes();
+        wireUpEvents();
+        initializeRating();
+    }
+
+    $(document).ready(function () {
+        if (document.getElementById('mdw-rating') !== null) {
+            // deck page with rating
+            initializeStars();
+        }
+        else {
+            // decklists page
+            updateRatingColumn();
         }
     });
-}
 
+}(jQuery));
+// End: Deck Ratings
+// ==========================================================================
