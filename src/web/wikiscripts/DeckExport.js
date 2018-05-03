9660bc34b42254f268595f34c1834f30b8806dbe
Aspallar <shadowthrone99@outlook.com>
2018-05-03 04:02:07 +0100

    ImportData added.

--- src/web/wikiscripts/DeckExport.js
+++ src/web/wikiscripts/DeckExport.js
@@ -12,8 +12,90 @@
 (function ($) {
     'use strict';
 
-    var importCards = [];
-    var altImportCards = [];
+    var importData;
+
+    function ImportData() {
+        var importCards = [];
+        var altImportCards = [];
+
+        function baseDisplayName(card) {
+            return card.name + ' (' + card.set + ') ' + card.cardNumber;
+        }
+
+        function importDisplayName(card) {
+            return card.num + ' ' + baseDisplayName(card);
+        }
+
+        function rarityValue(rarity) {
+            return { 'Common': 0, 'Uncommon': 1, 'Rare': 2, 'Mythic Rare': 3, 'Legendary': 5}[rarity];
+        }
+
+        function findImportCardByName(name) {
+            for (var k = 0, l = importCards.length; k < l; k++) {
+                if (importCards[k].name === name)
+                    return k;
+            }
+            return -1;
+        }
+
+        return {
+            parseCardData: function (dataString) {
+                 if (dataString !== null && dataString.length > 0)
+                    importCards = JSON.parse(dataString);
+            },
+            parseAltCardData: function (dataString) {
+                if (dataString !== null && dataString.length > 0) {
+                    altImportCards = JSON.parse(dataString);
+                    altImportCards.sort(function (a, b) {
+                        return a.name.localeCompare(b.name);
+                    });
+                }
+            },
+            swapCards: function (altCardIndex) {
+                var newCard = altImportCards[altCardIndex];
+                var oldCardIndex = findImportCardByName(newCard.name);
+                var oldCard = importCards[oldCardIndex];
+
+                newCard.num = oldCard.num;
+                altImportCards[altCardIndex] = oldCard;
+                importCards[oldCardIndex] = newCard;
+                return baseDisplayName(oldCard);
+            },
+            adjustToCheapest: function () {
+                for (var k = 0, l = altImportCards.length; k < l; k++) {
+                    var cardIndex = findImportCardByName(altImportCards[k].name);
+                    if (rarityValue(altImportCards[k].rarity) < rarityValue(importCards[cardIndex].rarity))
+                        this.swapCards(k);
+                }
+            },
+            getRarityTotals: function getRarityTotals() {
+                var totals = {};
+                importCards.forEach(function (card) {
+                    var rarity = card.rarity;
+                    if (rarity !== 'Basic Land')
+                        totals[rarity] = (totals[rarity] || 0) + card.num;
+                });
+                return totals;
+            },
+            text: function () {
+                var text = '';
+                importCards.forEach(function(card) {
+                    text += importDisplayName(card) + '\n';
+                });
+                return text;
+            },
+            getAltOptions: function () {
+                var options = document.createDocumentFragment();
+                altImportCards.forEach(function (card) {
+                    $('<option>').html(baseDisplayName(card)).appendTo(options);
+                });
+                return options;
+            },
+            hasAlternatives: function () {
+                return altImportCards.length > 0;
+            }
+        };
+    } // End ImportData
 
     function sizeTextareaToContents(textarea) {
         textarea.style.overflow = 'hidden';
@@ -22,8 +104,8 @@
     }
 
     function onClickCopy() {
-        var exportText = document.getElementById('mdw-arena-export-contents');
-        exportText.select();
+        var importText = document.getElementById('mdw-arena-export-contents');
+        importText.select();
         document.execCommand('copy');
         $('#mdw-copied-message').html(' copied to clipboard.');
         setTimeout(function () {
@@ -31,95 +113,34 @@
         }, 1200);
     }
 
-    function baseDisplayName(card) {
-        return card.name + ' (' + card.set + ') ' + card.cardNumber;
-    }
-
-    function importDisplayName(card) {
-        return card.num + ' ' + baseDisplayName(card);
-    }
-
-    function allImportText() {
-        var text = '';
-        importCards.forEach(function(card) {
-            text += importDisplayName(card) + '\n';
-        });
-        return text;
-    }
-
-    function findImportCardByName(name) {
-        for (var k = 0, l = importCards.length; k < l; k++) {
-            if (importCards[k].name === name)
-                return k;
-        }
-        return -1;
-    }
-
-    function getRarityTotals(cards) {
-        var totals = {};
-        cards.forEach(function (card) {
-            var rarity = card.rarity;
-            if (rarity !== 'Basic Land')
-                totals[rarity] = (totals[rarity] || 0) + card.num;
-        });
-        return totals;
-    }
-
-    function rarityValue(rarity) {
-        return { 'Common': 0, 'Uncommon': 1, 'Rare': 2, 'Mythic Rare': 3, 'Legendary': 5}[rarity];
-    }
-
-    function swapCards(altCardIndex) {
-        var newCard = altImportCards[altCardIndex];
-        var oldCardIndex = findImportCardByName(newCard.name);
-        var oldCard = importCards[oldCardIndex];
-
-        newCard.num = oldCard.num;
-        altImportCards[altCardIndex] = oldCard;
-        importCards[oldCardIndex] = newCard;
-        return baseDisplayName(oldCard);
-    }
-
-    function adjustImportToCheapest() {
-        for (var k = 0, l = altImportCards.length; k < l; k++) {
-            var cardIndex = findImportCardByName(altImportCards[k].name);
-            if (rarityValue(altImportCards[k].rarity) < rarityValue(importCards[cardIndex].rarity))
-                swapCards(k);
-        }
-    }
-
-
     function onSelectAlternative() {
         /* jshint -W040 */ // allow old school jquery use of this
-        var text = swapCards(this.selectedIndex - 1);
+        var text = importData.swapCards(this.selectedIndex - 1);
         this.options[this.selectedIndex].text = text;
-        $('#mdw-arena-export-contents').html(allImportText());
+        $('#mdw-arena-export-contents').html(importData.text());
         this.selectedIndex = 0;
-        console.dir(getRarityTotals(importCards));
+        console.dir(importData.getRarityTotals());
     }
 
-    function setupAlternativesSelect(container) {
-        if (container === null || altImportCards.length === 0)
+    function setupAlternativesUi(container) {
+        if (container === null || !importData.hasAlternatives())
             return;
 
         var altSelect = $('<select id="mdw-area-export-select"></select>')
             .append('<option disabled selected>Select alternative cards --</option>')
+            .append(importData.getAltOptions())
             .change(onSelectAlternative);
-
-        altImportCards.forEach(function (card) {
-            $('<option>').html(baseDisplayName(card)).appendTo(altSelect);
-        });
 
         $(container).append(altSelect).css('display', 'block');
     }
 
-    function setupExportText(container) {
+    function setupImportUi(container) {
         var elements = $(
                 '<input type="button" id="mdw-copy-export" value="Copy" />' +
                 '<span id="mdw-copied-message" />' +
                 '<br />' +
                 '<textarea id="mdw-arena-export-contents" style="width:90%" readonly>' +
-                allImportText() +
+                importData.text() +
                 '</textarea>'
         );
         $(container).append(elements);
@@ -129,29 +150,21 @@
 
     function initializeImportData()
     {
-        var dataString = $('#mdw-chartdata-pre').text();
-        if (dataString !== null && dataString.length > 0) {
-            importCards = JSON.parse(dataString);
-        }
-        dataString = $('#mdw-alt-carddata').text();
-        if (dataString !== null && dataString.length > 0) {
-            altImportCards = JSON.parse(dataString);
-            altImportCards.sort(function (a, b) {
-                return a.name.localeCompare(b.name);
-            });
-        }
-        adjustImportToCheapest();
-        console.dir(getRarityTotals(importCards));
+        importData = new ImportData();
+        importData.parseCardData($('#mdw-chartdata-pre').text());
+        importData.parseAltCardData($('#mdw-alt-carddata').text());
+        importData.adjustToCheapest();
+        console.dir(importData.getRarityTotals());
     }
 
     function initialize () {
-        var arenaExportContainer = document.getElementById('mdw-arena-export-div');
-        if (arenaExportContainer === null)
+        var arenaImportContainer = document.getElementById('mdw-arena-export-div');
+        if (arenaImportContainer === null)
             return;
 
         initializeImportData();
-        setupExportText(arenaExportContainer);
-        setupAlternativesSelect(document.getElementById('mdw-arena-export-alt-div'));
+        setupImportUi(arenaImportContainer);
+        setupAlternativesUi(document.getElementById('mdw-arena-export-alt-div'));
     }
 
     $(document).ready(initialize);
