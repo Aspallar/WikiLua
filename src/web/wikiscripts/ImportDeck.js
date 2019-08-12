9d141902ff08b24720f22313d0ce5c21d5bc85c6
Aspallar <shadowthrone99@outlook.com>
2019-08-12 14:21:27 +0100

    ImportDeck - Capitalize color strings in title + more attempts at blocking gibberish titles

--- src/web/wikiscripts/ImportDeck.js
+++ src/web/wikiscripts/ImportDeck.js
@@ -1,7 +1,7 @@
 // ==========================================================================
 // ImportDeck
 //
-// Version 1.4.0
+// Version 1.6.0
 // Author: Aspallar
 //
 // Provides a user friendly way to import a deck from Magic Arena
@@ -14,11 +14,36 @@
     /* global mw*/
     'use strict';
 
-    if (document.getElementById('mdw-import-deck') === null || $('#mdw-disabled-js').attr('data-importdeck-1-4-0'))
+    if (document.getElementById('mdw-import-deck') === null || $('#mdw-disabled-js').attr('data-importdeck-1-6-0'))
         return;
 
     var newDeckTemplate = '';
     var translation = null;
+    var titleCaser;
+
+
+    function TitleCaser(minorWords, acronyms) {
+
+        function isColorString(str) {
+            return str.length <= 5 && 
+                ['rug', 'rub', 'bug', 'grub'].indexOf(str) == -1 &&
+                /^(?:([ubwrg])(?!.*\1))*$/.test(str);
+        }
+
+        return {
+            caseText: function(text) {
+                text = text.trim().toLowerCase();
+                return text.replace(/\w+/g, function(word, offset) {
+                    if ((acronyms && acronyms.indexOf(word) !== -1) || isColorString(word))
+                        return word.toUpperCase();
+                    else if (minorWords && offset !== 0 && minorWords.indexOf(word) !== -1)
+                        return word.toLowerCase();
+                    else
+                        return word.substring(0, 1).toUpperCase() + word.substring(1);
+                });                
+            }
+        };
+    }
 
     function removeIncludes(contents) {
         return contents.replace(/<[\/]?noinclude>|<[\/]?includeonly>/g, '');
@@ -41,12 +66,8 @@
     }
 
     function showBadEntries(badEntries) {
-        var safeBadEntries = [];
-        badEntries.forEach(function (entry) {
-            safeBadEntries.push(mw.html.escape(entry));
-        });
-        var content = safeBadEntries.join('<br />');
-        $('#mdw-import-badentries-content').html(content);
+        var safeContent = badEntries.map(function (x) { return mw.html.escape(x); }).join('<br />');
+        $('#mdw-import-badentries-content').html(safeContent);
         $('#mdw-import-badentries').fadeIn(400);
     }
 
@@ -91,8 +112,16 @@
         var name = $('#mdw-import-deckname').val().trim();
         if (name.length === 0)
             return displayError('deckname', 'A deck name is required.');
+        else if (/^\d+$/.test(name))
+            return displayError('deckname', 'Deck name cannot just be a number.');
+        else if (name.length < 3)
+            return displayError('deckname', 'Deck name is too short.');
         else if (invalidTitle(name))
             return displayError('deckname', 'Deck name must not contain /, #, or ? ');
+        else if (/(.)\1{2,}/i.test(name))
+            return displayError('deckname', 'Too many repeating characters.');
+        else if (/^[qwerty ]+$|^[asdf ]+$|^[zxcvb ]$/i.test(name))
+            return displayError('deckname', 'Looks like gibberish, try another name.');
         else
             return true;
     }
@@ -145,11 +174,9 @@
     }
 
     function getDeckText(deckEntries) {
-        var translated = [];
-        deckEntries.forEach(function (entry) {
-            translated.push(translateCard(entry));
-        });
-        return translated.join('\n');
+        return deckEntries.map(function (entry) {
+            return translateCard(entry);
+        }).join('\n');
     }
 
     function createDeckPage(name, deckEntries, originalText) {
@@ -168,18 +195,27 @@
                 token: mw.user.tokens.get('editToken')
             }).done(function(result) {
                 hideWorking();
-                if (result.error === undefined) {
+                if (result.error === undefined)
                     redirectToTitle(title);
-                } else {
+                else
                     handleCreateError(result.error);
-                }
             }).fail(function(code, result) {
                 unexpectedError(code + (code === 'http' ? ' ' + result.textStatus : ''));
             });
         });
     }
 
+    function disallowedRegex() {
+        var disallowed = $('#mdw-import-deck').attr('data-disallow');
+        return disallowed ? new RegExp('[' + disallowed.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + ']') : null;
+    }
+
+    function stripSetCode(cardEntry) {
+        return cardEntry.replace(/\([A-Z][0-9A-Z][0-9A-Z]\)/, '');
+    }
+
     function parseDeckDef(deckdef) {
+        var disallowed = disallowedRegex();
         var maxLength = $('#mdw-import-deck').attr('data-maxcardlength') || 40;
         var result = { validEntries: [], badEntries: [], sideboardCount: 0 };
         deckdef.split('\n').forEach(function (entry) {
@@ -189,6 +225,8 @@
                 result.validEntries.push('--- sideboard ---');
             } else if (entry.length > maxLength) {
                 result.badEntries.push(entry.substring(0, maxLength) + '... (too long)');
+            } else if (disallowed && disallowed.test(stripSetCode(entry))) {
+                result.badEntries.push(entry);
             } else if (/^\d+\s+.*\S.*$/.test(entry)) {
                 result.validEntries.push(correctCardName(entry));
             } else {
@@ -261,10 +299,18 @@
         }
     }
 
+    function makeTitleCase() {
+        /*jshint -W040 */ // allow old school jquery use of this 
+        var that = $(this);
+        that.val(titleCaser.caseText(that.val()));
+    }
+
     function createForm() {
         /*jshint multistr:true*/
-        $('#mdw-import-deckname-span')
-            .append('<input type="text" id="mdw-import-deckname" size="40" maxlength="100" placeholder="Deck name" />');
+        $('#mdw-import-deckname-span').append(
+            $('<input type="text" id="mdw-import-deckname" size="40" maxlength="100" placeholder="Deck name" />')
+                .blur(makeTitleCase)
+        );
         $('#mdw-import-deckdef-span')
             .append('<textarea id="mdw-import-deckdef" cols="60" rows="25"></textarea>');
         $('#mdw-import-button-span')
@@ -286,6 +332,8 @@
         }));
         showWorking();
         fetchNewDeckTemplate().done(function (template) {
+            var importDef = $('#mdw-import-deck');
+            titleCaser = new TitleCaser((importDef.attr('data-minorwords') || '').split('|'), (importDef.attr('data-acronyms') || '').split('|'));
             newDeckTemplate = removeIncludes(template);
             createForm();
             hideWorking();
