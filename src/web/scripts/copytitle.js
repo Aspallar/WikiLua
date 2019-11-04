e0a4a1c37c15d275757bc8a8c87fd140e24b5770
Aspallar <shadowthrone99@outlook.com>
2019-11-04 17:10:37 +0000

    ctrl+click for copy title

--- src/web/scripts/copytitle.js
+++ src/web/scripts/copytitle.js
@@ -1,11 +1,14 @@
 // copy article title to clipboard when title clicked.
 $(function (){
     'use strict';
-    $('h1.page-header__title').css('cursor', 'pointer').click(function () {
+    $('h1.page-header__title').css('cursor', 'pointer').click(function (event) {
         var that = $(this);
         var color = that.css('color');
-        that.css('color','red');
-        navigator.clipboard.writeText(that.text()).then(function () {
+        that.css('color', 'red');
+        var copyText = that.text();
+        if (!event.ctrlKey)
+            copyText = 'Duplicate of [[' + copyText + ']]'
+        navigator.clipboard.writeText(copyText).then(function () {
             setTimeout(function () {
                 that.css('color', color);
             }, 200);
