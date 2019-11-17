de14e1279ca653675f80e6278ebc45c5823ecafb
Aspallar <shadowthrone99@outlook.com>
2019-11-17 15:28:44 +0000

    Removed unused files

--- src/web/wikiscripts/OpenInNewTab.js
+++
@@ -1,9 +0,0 @@
-mw.hook('wikipage.content').add(function($content) {
-    console.log('one');
-    if ($content.selector === '#mw-content-text') {
-        console.log('two');
-        var openInNewTab = $content.find('.mdw-newtab');
-        if (openInNewTab.length > 0)
-            openInNewTab.find('a').attr('target', '_blank');
-    }
-});
