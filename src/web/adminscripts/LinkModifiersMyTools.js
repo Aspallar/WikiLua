a713eec184b6f2c29c6af1dd86fb43b1828792e9
Aspallar <shadowthrone99@outlook.com>
2019-11-25 19:33:39 +0000

    OpenBlank replaced with LinkModifiersMyTools

---
+++ src/web/adminscripts/LinkModifiersMyTools.js
@@ -0,0 +1,36 @@
+(function ($) {
+    'use strict';
+    function highlight(element) {
+        $(element).css('font-weight', 'bold');
+    }
+    function openBlank(event) {
+        /*jshint -W040 */
+        event.preventDefault();
+        highlight(this);
+        $('#WikiaArticle a').attr('target', '_blank');
+    }
+    function openRaw(event) {
+        /*jshint -W040 */
+        event.preventDefault();
+        highlight(this);
+        var actionRe = /[\?&]action=/;
+        $('#WikiaArticle a').each(function () {
+            var that = $(this);
+            var href = that.attr('href');
+            if (href !== '#' && !actionRe.test(href)) {
+                that.attr('href', href + (href.includes('?') ? '&' : '?') + 'action=raw');
+            }
+        });
+    }
+    function menuLink(text, handler) {
+        return $('<li>')
+            .append($('<a>').attr({href: '#'}).text(text))
+            .click(handler);
+    }
+    $(function () {
+        $('#my-tools-menu').prepend(
+            menuLink('Open Blank', openBlank),
+            menuLink('Open Raw', openRaw)
+        );
+    });
+}(jQuery));
