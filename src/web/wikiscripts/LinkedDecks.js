baa64aa6a5eeb85648165b374c499fa2cb69a816
Aspallar <shadowthrone99@outlook.com>
2020-06-03 17:48:39 +0100

    Use mw.api for api calls

--- src/web/wikiscripts/LinkedDecks.js
+++ src/web/wikiscripts/LinkedDecks.js
@@ -2,7 +2,7 @@
 // Implements a link on card pages that displays links to the decks which
 // contain that card.
 //
-// Version 1.0.1
+// Version 1.0.2
 // Author: Aspallar
 //
 // ** Please do not edit this code directly in the wikia.
@@ -12,25 +12,16 @@
     'use strict';
     /*global mw */
 
-    if (document.getElementById('mdw-deck-links') === null || $('#mdw-disabled-js').attr('data-linkeddecks-1-0-1'))
+    if (document.getElementById('mdw-deck-links') === null || $('#mdw-disabled-js').attr('data-linkeddecks-1-0-2'))
         return;
 
-    function wikiApiCall(data, method) {
-        data.format = 'json';
-        return $.ajax({
-            data: data,
-            dataType: 'json',
-            url: mw.config.get('wgScriptPath') + '/api.php',
-            type: method
-        });
-    }
-
     function fetchDecks() {
-        var deferred = $.Deferred();
-        var decks = [];
+        var deferred = $.Deferred(),
+            api = new mw.Api(),
+            decks = [];
 
         function getDecks(blcontinue) {
-            wikiApiCall({
+            api.get({
                 action: 'query',
                 list: 'backlinks',
                 bltitle: mw.config.get('wgTitle'),
@@ -38,10 +29,10 @@
                 rawcontinue: '',
                 blfilterredir: 'nonredirects',
                 blcontinue: blcontinue
-            }, 'GET').done(function (data) {
-                if (data.query.backlinks) {
+            }).done(function (data) {
+                if (data.query && data.query.backlinks) {
                     data.query.backlinks.forEach(function (deck) {
-                        if (deck.title.substring(0, 6) === 'Decks/')
+                        if (/^Decks\//.test(deck.title))
                             decks.push(deck.title);
                     });
                 }
@@ -53,14 +44,13 @@
             });
         }
 
-        getDecks(undefined);
+        getDecks();
         return deferred.promise();
     }
 
     function showDecks(event) {
         event.preventDefault();
-        var container = $('#mdw-deck-links');
-        container.empty();
+        var container = $('#mdw-deck-links').empty();
         fetchDecks().done(function (decks) {
             if (decks.length === 0)
                 container.html('There are no decks that contain this card.');
@@ -68,23 +58,23 @@
                 var decklinks = $(document.createDocumentFragment());
                 decks.sort();
                 decks.forEach(function (deck) {
-                    decklinks.append(
-                        $('<a>')
-                            .attr('href', mw.util.getUrl(deck))
-                            .attr('target', '_blank')
-                            .html(deck.substring(6))
-                    );
-                    decklinks.append('<br>');
+                    decklinks.append($('<a>', {
+                        href: mw.util.getUrl(deck),
+                        target: '_blank',
+                        html: deck.substring(6)
+                    })).append('<br>');
                 });
                 container.append(decklinks);
             }
         });
     }
 
-    $(document).ready(function () {
-        $('#mdw-deck-links').append(
-            $('<a>').attr('href', '#').html('Show decks that contain this card.').click(showDecks)
-        );
+    mw.loader.using('mediawiki.api', function () {
+        $(function () {
+            $('#mdw-deck-links').append(
+                $('<a>', {href: '#', html: 'Show decks that contain this card.', click: showDecks})
+            );
+        });
     });
 
 }(jQuery));
